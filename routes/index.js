d6da9b5e0ecbad912497d98afca572e4ed62a7be
Hitoki <melendrezomar2@gmail.com>
2021-05-18 18:09:59 -0300

    terminando workshop

--- routes/index.js
+++ routes/index.js
@@ -7,15 +7,18 @@ module.exports = router;
 
 // una función reusable
 function respondWithAllTweets(req, res, next) {
-	client.query("SELECT * FROM tweets", (err, data) => {
-		if (err) return next(err);
-		var allTheTweets = data.rows;
-		res.render("index", {
-			title: "Twitter.js",
-			tweets: allTheTweets,
-			showForm: true,
-		});
-	});
+	client.query(
+		"SELECT * FROM tweets 	JOIN users ON tweets.user_id = users.id",
+		(err, data) => {
+			if (err) return next(err);
+			var allTheTweets = data.rows;
+			res.render("index", {
+				title: "Twitter.js",
+				tweets: allTheTweets,
+				showForm: true,
+			});
+		}
+	);
 }
 
 // aca basícamente tratamos a la root view y la tweets view como identica
@@ -25,7 +28,7 @@ router.get("/tweets", respondWithAllTweets);
 // página del usuario individual
 router.get("/users/:username", function (req, res, next) {
 	client.query(
-		"SELECT tweets.content FROM tweets INNER JOIN users ON users.id = tweets.user_id WHERE users.name=$1",
+		"SELECT * FROM tweets INNER JOIN users ON users.id = tweets.user_id WHERE users.name=$1",
 		[req.params.username],
 		(err, data) => {
 			if (err) return next(err);
@@ -42,42 +45,59 @@ router.get("/users/:username", function (req, res, next) {
 
 // página del tweet individual
 router.get("/tweets/:id", function (req, res, next) {
-	client.query("SELECT tweets.content FROM tweets WHERE tweets.id = $1", [req.params.id], (err, data) => {
-		if(err) return next(err);
-		var tweetsWithThatId = data.rows;
-		res.render("index", {
-			title: "Twitter.js",
-			tweets: tweetsWithThatId, // un arreglo de solo un elemento ;-)
-		});
-	})
+	client.query(
+		"SELECT * FROM tweets INNER JOIN users ON users.id = tweets.user_id WHERE tweets.id = $1",
+		[req.params.id],
+		(err, data) => {
+			if (err) return next(err);
+			var tweetsWithThatId = data.rows;
+			res.render("index", {
+				title: "Twitter.js",
+				tweets: tweetsWithThatId, // un arreglo de solo un elemento ;-)
+			});
+		}
+	);
 });
 
 // crear un nuevo tweet
 router.post("/tweets", function (req, res, next) {
-	const variables = [req.body.name, req.body.content]
-	const exist = client.query('SELECT * FROM users WHERE users.name = $1', [variables[0]], (err, data) => {
-		if(err) return next(err);
-		return data.rows.length !== 0;
-	})
-	if(exist){
-		client.query("INSERT INTO tweets (user_id, content) VALUES ((SELECT id from users where name = $1), $2)", variables, (err, data) => {
-			if(err) return next(err);
-			res.redirect("/");
-		})
+	const variables = [req.body.name, req.body.content];
+	const exist = client.query(
+		"SELECT * FROM users WHERE users.name = $1",
+		[variables[0]],
+		(err, data) => {
+			if (err) return next(err);
+			return data.rows.length !== 0;
+		}
+	);
+	if (exist) {
+		client.query(
+			"INSERT INTO tweets (user_id, content) VALUES ((SELECT id from users where name = $1), $2)",
+			variables,
+			(err, data) => {
+				if (err) return next(err);
+				res.redirect("/");
+			}
+		);
 	} else {
 		//
-		client.query("INSERT INTO users (name, picture_url) VALUES ($1, 'http://i.imgur.com/JKInSVz.jpg');", [variables[0]], (err, data) => {
-			if(err) return next(err);
-		})
+		client.query(
+			"INSERT INTO users (name, picture_url) VALUES ($1, 'http://i.imgur.com/JKInSVz.jpg');",
+			[variables[0]],
+			(err, data) => {
+				if (err) return next(err);
+			}
+		);
 
-		client.query("INSERT INTO tweets (user_id, content) VALUES ((SELECT id from users where name = $1), $2)", variables, (err, data) => {
-			if(err) return next(err);
-			res.redirect("/");
-		})
+		client.query(
+			"INSERT INTO tweets (user_id, content) VALUES ((SELECT id from users where name = $1), $2)",
+			variables,
+			(err, data) => {
+				if (err) return next(err);
+				res.redirect("/");
+			}
+		);
 	}
-
-
-
 
 	//INSERT INTO tweets (user_id, content) VALUES ((SELECT id from users where name='Kanye West'),          'I think what Kanye West is going to mean is something similar to what Steve Jobs means.');
 });
